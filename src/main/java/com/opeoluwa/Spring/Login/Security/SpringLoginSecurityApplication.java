7349187d6ab95738bffb8b45902d172775bfdcb4
ayoade opeoluwa <ayoadeopeoluwa@yahoo.com>
2023-12-08 23:38:11 +0100

    initailize

---
+++ src/main/java/com/opeoluwa/Spring/Login/Security/SpringLoginSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.opeoluwa.Spring.Login.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opeoluwa/Spring/Login/Security/SpringLoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeoluwa.Spring.Login.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
